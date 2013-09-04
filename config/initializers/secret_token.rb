ed7ad524bf5bc61b14dcda0be4fc9d1b8a18d652
Clay Ashby <clay.ashby@gmail.com>
2013-09-04 13:09:02 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Med1::Application.config.secret_key_base = '6ff6bbb21a9a55126172ed6eff259a574d778aa31d5431d20d28f1548f20d9f0decac8bb4696068d34c4633c99c17bd91fd460f2a9cb115651d15e777459631b'
