43df4323f5b3c15daf063b9baf5e3f145a02f2cc
Cap Kello <captainkello@gmail.com>
2013-09-04 18:26:39 -0700

    added show view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,21 @@ class TasksController < ApplicationController
   	@task = Task.new
   end
 
+  def create
+  	@task = Task.new(task_params)
+  	@task.save
+  	redirect_to action: :show, id: @task.id
+  end
+
+  def show
+  	@task = Task.find(params[:id])
+  end
+
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+    def task_params
+    	params.require(:task).permit(:title)
+    end
+
 end
